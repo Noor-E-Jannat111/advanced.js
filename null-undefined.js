97b01205ad55c5b00a27379ce48e087d978b4ea3
Noor-E-Jannat123 <noorejannat188@gmail.com>
2021-02-02 00:03:55 +0600

    null-undefined

---
+++ null-undefined.js
@@ -0,0 +1,23 @@
+// let pakhi;
+
+// function add(num1, num2) {
+//     console.log(num1 + num2);
+//     return
+// }
+
+// function add(num1, num2) {
+//     console.log(num1, num2);
+// }
+
+
+// const pall = {
+//     name: "smart dude",
+//     phone: 2345555
+// };
+// console.log(pall.job);
+
+let fun = undefined;
+console.log(fun);
+
+let ages = [2, 5, 67];
+console.log(ages[11]);

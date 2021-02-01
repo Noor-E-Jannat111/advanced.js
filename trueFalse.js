8e25a3601670e92c5ee592d5bb86ee4f0938e42d
Noor-E-Jannat123 <noorejannat188@gmail.com>
2021-02-01 23:43:18 +0600

    true-false

---
+++ trueFalse.js
@@ -0,0 +1,22 @@
+// FALSE:
+// false
+// 0
+// null
+// undefined
+// NaN
+// ''/""
+
+TRUE:
+    // " ",
+    // '0',
+    // {},
+    // [],
+
+
+
+    const age = 4;
+if (age > 0) {
+    console.log("condition is true");
+} else {
+    console.log("condition is false");
+}

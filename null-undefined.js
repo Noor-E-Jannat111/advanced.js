5ee63e42bf26af7c872128d049dcc917ffc9c4a0
Noor-E-Jannat123 <noorejannat188@gmail.com>
2021-02-02 00:54:38 +0600

    map-filter

--- null-undefined.js
+++ null-undefined.js
@@ -1,20 +1,20 @@
-// let pakhi;
+let pakhi;
 
-// function add(num1, num2) {
-//     console.log(num1 + num2);
-//     return
-// }
+function add(num1, num2) {
+    console.log(num1 + num2);
+    return
+}
 
-// function add(num1, num2) {
-//     console.log(num1, num2);
-// }
+function add(num1, num2) {
+    console.log(num1, num2);
+}
 
 
-// const pall = {
-//     name: "smart dude",
-//     phone: 2345555
-// };
-// console.log(pall.job);
+const pall = {
+    name: "smart dude",
+    phone: 2345555
+};
+console.log(pall.job);
 
 let fun = undefined;
 console.log(fun);

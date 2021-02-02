d294e01df070991ab82112ec20db418b56adaae0
Noor-E-Jannat123 <noorejannat188@gmail.com>
2021-02-02 13:46:52 +0600

    array-spllit

---
+++ array-spllit.js
@@ -0,0 +1,9 @@
+ const num = [1, 2, 3, 4, 5, 6, 7, 8];
+ // const part = num.slice(2, 5);
+
+ const removed = num.splice(2, 5, 9);
+ //  console.log(removed);
+ //  console.log(num);
+
+ const together = num.join(",");
+ console.log(together);
